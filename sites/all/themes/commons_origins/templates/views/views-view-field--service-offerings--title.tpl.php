262fce85ffdc24cb1e8fa0a8e71366d3c5039e58
Gowri A <gowri@Panaromic.local>
2013-05-24 21:14:34 -0700

    CAP, Search and all dynamic forms implemented

--- sites/all/themes/commons_origins/templates/views/views-view-field--service-offerings--title.tpl.php
+++ sites/all/themes/commons_origins/templates/views/views-view-field--service-offerings--title.tpl.php
@@ -23,15 +23,16 @@
  */
 ?>
 <?php 
-      global $user,$base_url;
+      global $user,$base_url,$theme_path;
       $uid = $user->uid;
-// find out whether there is any matching available for this need nid
+// find out whether there is any matching available for this resource nid
       $fields = explode("/",$output);
+      $title = drupal_substr($fields[2],0,20).'...';
       $ret = ghn_resource_matching($uid,$fields[1]);
       if (count($ret) > 0) {
-         print '<a href="'.$base_url.'/node/'.$fields[1].'/edit" title="'.$fields[2].'">'.$fields[2].'</a> <a class="match-link" href="'.$base_url.'/match/need/'.$uid.'/'.$fields[1].'">Matched</a>'; 
+         print '<a href="'.$base_url.'/node/'.$fields[1].'/edit" title="'.$fields[2].'">'.$title.'</a> <a class="match-link" href="'.$base_url.'/match/resource/'.$uid.'/'.$fields[1].'"><img src="'.$base_url.'/'.$theme_path.'/images/checkMark.png"></a>'; 
       }
       else {
-         print '<a href="'.$base_url.'/node/'.$fields[1].'/edit" title="'.$fields[2].'">'.$fields[2].'</a> NO MATCH'; 
+         print '<a href="'.$base_url.'/node/'.$fields[1].'/edit" title="'.$fields[2].'">'.$title.'</a><img src="'.$base_url.'/'.$theme_path.'/images/xMark.png">'; 
       }
 ?>
